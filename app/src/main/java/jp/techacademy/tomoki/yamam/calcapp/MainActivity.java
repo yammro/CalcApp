c93b83d96392a79f6eacfdf9a709d04c37038805
yammro <yamamuromuro@gmail.com>
2018-02-09 15:46:57 +0900

    課題：四則演算アプリを提出します。 フラグとして、fugouを使用し、1:＋,2:－,3:×,4:÷としています

--- app/src/main/java/jp/techacademy/tomoki/yamam/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/tomoki/yamam/calcapp/MainActivity.java
@@ -4,24 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText mEditText1,mEditText2;
+    int fugou;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
+        mEditText1 = (EditText) findViewById(R.id.editText1);
+        mEditText2 = (EditText) findViewById(R.id.editText2);
+        Button tasu_button = (Button) findViewById(R.id.tasu_button);
+        Button hiku_button = (Button) findViewById(R.id.hiku_button);
+        Button kakeru_button = (Button) findViewById(R.id.kakeru_button);
+        Button waru_button = (Button) findViewById(R.id.waru_button);
+        tasu_button.setOnClickListener(this);
+        hiku_button.setOnClickListener(this);
+        kakeru_button.setOnClickListener(this);
+        waru_button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+
+        if (v.getId() == R.id.tasu_button) {
+            fugou = 1;   // fugou 1：＋, 2：－,3：×,4：÷
+        } else if (v.getId() == R.id.hiku_button) {
+            fugou = 2;
+        } else if (v.getId() == R.id.kakeru_button) {
+            fugou = 3;
+        } else if (v.getId() == R.id.waru_button) {
+            fugou = 4;
+        }
+        Log.d("javatest", String.valueOf(fugou)); // デバッグ用に追加する
+        Log.d("javatest", String.valueOf(mEditText1.getText().toString())); // デバッグ用に追加する
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("VALUE1", 10);
-        intent.putExtra("VALUE2", 20);
+        intent.putExtra("VALUE1", Double.parseDouble(mEditText1.getText().toString()));
+        intent.putExtra("VALUE2", Double.parseDouble(mEditText2.getText().toString()));
+        intent.putExtra("VALUE3", fugou);
         startActivity(intent);
     }
 }
--- app/src/main/java/jp/techacademy/tomoki/yamam/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/tomoki/yamam/calcapp/SecondActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,10 +14,20 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Intent intent = getIntent();
-        int value1 = intent.getIntExtra("VALUE1", 0);
-        int value2 = intent.getIntExtra("VALUE2", 0);
-
+        double value1 = intent.getDoubleExtra("VALUE1", 0); //小数点にも対応
+        double value2 = intent.getDoubleExtra("VALUE2", 0);
+        int value3 = intent.getIntExtra("VALUE3", 0);
+        Log.d("javatest", String.valueOf(value3)); // デバッグ用に追加する
+        Log.d("javatest", String.valueOf(value1)); // デバッグ用に追加する
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(String.valueOf(value1 + value2));
+        if(value3==1){  // fugou(value3=) 1：＋, 2：－,3：×,4：÷
+            textView.setText(String.valueOf(value1 + value2));
+        }else if(value3==2){
+            textView.setText(String.valueOf(value1 - value2));
+        }else if(value3==3){
+            textView.setText(String.valueOf(value1 * value2));
+        }else if(value3==4){
+            textView.setText(String.valueOf(value1 / value2));
+        }
     }
 }
